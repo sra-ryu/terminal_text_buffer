16d1b78368e3c372d5347fcd597dc940256b5be8
Saera Ryu <ryu@mac.speedport.ip>
2026-03-11 22:53:50 +0100

    feat: initialize Cursor structure

--- src/main/java/org/example/cursor/Cursor.java
+++ src/main/java/org/example/cursor/Cursor.java
@@ -1,4 +1,16 @@
 package org.example.cursor;
 
+import org.example.model.CellAttributes;
+
 public class Cursor {
+    private int col, row;
+    private final int maxWidth;
+    private final int maxHeight;
+
+    public Cursor(int maxWidth, int maxHeight) {
+        this.col = 0;
+        this.row = 0;
+        this.maxWidth = maxWidth;
+        this.maxHeight = maxHeight;
+    }
 }
