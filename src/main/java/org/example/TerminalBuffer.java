6515ed94273b251e91acbb17127d1f3d45343e8e
Saera Ryu <ryu@mac.speedport.ip>
2026-03-11 22:41:46 +0100

    feat: add additional field and change contructor

--- src/main/java/org/example/TerminalBuffer.java
+++ src/main/java/org/example/TerminalBuffer.java
@@ -1,15 +1,28 @@
 package org.example;
 
+import org.example.model.BufferLine;
 import org.example.model.Cell;
+
+import java.util.*;
 
 public class TerminalBuffer {
     private final int width;
     private final int height;
-    private final Cell[][] grid;
+    private final int scrollBackMaxSize;
 
-    public TerminalBuffer(int width, int height, Cell[][] grid) {
+    private final List<BufferLine> screen;
+
+    private final Deque<BufferLine> scrollback;
+
+    public TerminalBuffer(int width, int height, int scrollBackMaxSize) {
         this.width = width;
         this.height = height;
-        this.grid = new Cell[height][width];
+        this.scrollBackMaxSize = scrollBackMaxSize;
+        this.screen = new ArrayList<>(height);
+        this.scrollback = new ArrayDeque<>(scrollBackMaxSize);
+
+        for (int i = 0; i < height; i++) {
+            screen.add(new BufferLine(width));
+        }
     }
 }
