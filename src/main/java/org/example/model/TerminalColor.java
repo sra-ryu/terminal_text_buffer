bb2dfd1fb72ee76d9369ddf34c67896f5dabaeeb
Saera Ryu <ryu@mac.speedport.ip>
2026-03-11 17:18:23 +0100

    feat: add TerminalColor enum for Cell

--- src/main/java/org/example/model/TerminalColor.java
+++ src/main/java/org/example/model/TerminalColor.java
@@ -1,4 +1,7 @@
 package org.example.model;
 
-public class TerminalColor {
+public enum TerminalColor {
+    DEFAULT, BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE,
+    BRIGHT_BLACK, BRIGHT_RED, BRIGHT_GREEN, BRIGHT_YELLOW, BRIGHT_BLUE,
+    BRIGHT_MAGENTA, BRIGHT_CYAN, BRIGHT_WHITE
 }
